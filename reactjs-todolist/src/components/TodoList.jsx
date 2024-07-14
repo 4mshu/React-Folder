d9c664dcfeb00b15894afb6a7c5fd51d7da51421
4mshu <2hungrygators@gmail.com>
2024-07-14 15:42:51 +0545

    add to list done; todo add logic for edit and delete

--- reactjs-todolist/src/components/TodoList.jsx
+++ reactjs-todolist/src/components/TodoList.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import TodoCard from './TodoCard'
 
-export default function TodoList() {
-  let todos =[
-    'go to the gym',
-    'eat more',
-    'bla bla bla'
-  ]
+export default function TodoList(props) {
+  const {todos} = props;
+
   return (
     <ul className='main'>
       {todos.map((todo,todoIndex)=>{
