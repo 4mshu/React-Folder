acfd999e142492016161c7bc62b56cb1e878cfc1
4mshu <2hungrygators@gmail.com>
2024-07-14 20:09:26 +0545

    todo list completed

--- reactjs-todolist/src/App.jsx
+++ reactjs-todolist/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import TodoInput from "./components/TodoInput"
 import TodoList from "./components/TodoList"
 function App() {
@@ -6,15 +6,21 @@ function App() {
 const [todos, setTodos] = useState([]);
 const [todoValue, setTodoValue] = useState('')
 
+function persistData(newList){
+  localStorage.setItem('todos', JSON.stringify({todos:newList}))
+}
+
 function handleAddTodos(newTodo){
-  const newTodoList = [...todos, newTodo];
-  setTodos(newTodoList);
+  const newTodoList = [...todos, newTodo]
+  persistData(newTodoList)
+  setTodos(newTodoList)
 }
 
 function handleDeleteTodo(index){
   const newTodoList = todos.filter((todo,todoIndex)=>{
     return todoIndex !== index
   })
+  persistData(newTodoList)
   setTodos(newTodoList);
 }
 
@@ -24,6 +30,19 @@ function handleEditTodo(index){
   handleDeleteTodo(index)
 
 }
+
+useEffect(() => {
+  if(!localStorage){
+    return
+  }
+  let localTodos = localStorage.getItem('todos')
+  if (!localTodos){
+    return
+  }
+  localTodos = JSON.parse(localTodos).todos
+  setTodos(localTodos)
+
+}, [])
 
   return (
     <>
